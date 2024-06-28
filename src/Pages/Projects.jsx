b785f75689d55181a0a171b628f0c26025393220
osher volotker <volotkero@gmail.com>
2024-06-29 01:18:55 +0300

    -created project page and css -added images to project page

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import "../Styles/Projects.css";
 
 function Projects() {
     return (
-      <div>
-        <h1>Welcome to the Projects Page</h1>
-        <p>This is the home page of your React app.</p>
+      <div className="project-page">
+      <div className='field'>
+        <h1>Career Projects: <p>career projects are fields for hire or collaboration.</p></h1>
+        <h1>Impression Projects: <p>impression projects are showcase of my skills, and not looking for work in those fields.</p></h1>
       </div>
+      <div className="category gaming">
+        <h2>Gaming</h2>
+        <h3>Career Projects</h3>
+        <div></div>
+        
+      </div>
+      <div className="category web">
+        <h2>Web</h2>
+        <h3>Impression Projects</h3>
+        <div></div>
+      </div>
+      <div className="category film">
+        <h2>Film</h2>
+        <h3>Career Projects</h3>
+        <div></div>
+      </div>
+      <div className="category writing">
+        <h2>Literature</h2>
+        <h3>Impression Projects</h3>
+        <div></div>
+      </div>
+    </div>
     );
   }
 
